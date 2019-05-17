43fbc15ab9ef338f5d85459e133501207acb355c
Albert Chung <chung972@gmail.com>
2019-05-17 13:28:33 -0500

    fixes typo in routes and view for destination

--- routes/destinations.js
+++ routes/destinations.js
@@ -2,6 +2,6 @@ var express = require("express");
 var router = express.Router();
 var destCtrl = require("../controllers/destinations");
 
-router.post("/flights/:id/desintations", destCtrl.create);
+router.post("/flights/:id/destinations", destCtrl.create);
 
 module.exports = router;
