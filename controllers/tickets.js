8c652593bdf52aef38b49c4630ea3836d33de117
Albert Chung <chung972@gmail.com>
2019-05-20 07:29:53 -0500

    adds newTicket and creates functionality

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,14 +1,22 @@
 const Ticket = require("../models/ticket");
+const Flight = require("../models/flight");
 
 module.exports = {
     new: newTicket,
     create
 }
 
-function newTicket(req, res){
-
+function newTicket(req, res) {
+    Flight.findById(req.params.id, (err, flight) => {
+        res.render("tickets/new", {
+            title: "New Ticket",
+            flight
+        });
+    });
 }
 
-function create(req, res){
-    
+function create(req, res) {
+    Ticket.create(req.body, (err, ticket)=>{
+        res.redirect(`/flights/${ticket.flight}`);
+    });
 }
