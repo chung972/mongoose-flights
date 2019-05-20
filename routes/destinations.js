afe09074d62a13f1ab0c7c7ecf767886153e524e
Albert Chung <chung972@gmail.com>
2019-05-20 12:18:14 -0500

    adds comments on how to refactor var express and router

--- routes/destinations.js
+++ routes/destinations.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var router = express.Router();
+// ALTERNATIVELY, you could also refactor the two  lines above into:
+//      const router = require("express").Router();
+// the only use case for keeping two separate variables is if you plan on 
+// referencing either of them separately somewhere in the code below
 var destCtrl = require("../controllers/destinations");
 
 router.post("/flights/:id/destinations", destCtrl.create);
