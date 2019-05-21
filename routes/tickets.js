c6074f099a8caa46303d6b0c65287efb6afbb768
Albert Chung <chung972@gmail.com>
2019-05-20 19:05:37 -0500

    fixes UN-RESTful routes to RESTful routes

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const ticketCtrl = require("../controllers/tickets");
+const ticketsCtrl = require("../controllers/tickets");
 
-router.get("/flights/:id/tickets/new", ticketCtrl.new);
-router.post("/tickets", ticketCtrl.create);
+router.get("/flights/:id/tickets/new", ticketsCtrl.new);
+// router.post("/tickets", ticketCtrl.create);
+// again, i want to emphasize that the above would UN-RESTful routing; this is
+// beCAUSE we are dealing with RELATED DATA RELATIONSHIPS, which demand a
+// DIFFERENT set of routes: https://gist.github.com/jim-clark/d7e5c9130c9b46b253d4c47aa601596a
+router.post("/flights/:id/tickets", ticketsCtrl.create);
 // remember, routes map requests to code
 
 module.exports = router;
