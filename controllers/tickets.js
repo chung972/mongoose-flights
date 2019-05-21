8e5e87cf10176834ace10c8778634e8192099580
Albert Chung <chung972@gmail.com>
2019-05-20 19:03:21 -0500

    fixes UN-RESTful routing; adds appropriate comments explaining changes

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -16,7 +16,19 @@ function newTicket(req, res) {
 }
 
 function create(req, res) {
+    req.body.flight = req.params.id
+    // req.params.id will return the ._id of the flight object that was passed in to 
+    // tickets/new.ejs (just see the function above). because we never ask for the flight
+    // id from the user, we have to ADD that property (this can be done using dot (.) notation)
+    // and assign it a value of req.params.id above. Remember, this must be done BEFORE calling
+    // .create on the Ticket model
     Ticket.create(req.body, (err, ticket)=>{
-        res.redirect(`/flights/${ticket.flight}`);
+
+        // res.redirect(`/flights/${ticket.flight}`);
+        // again, the above is an artifact of UN-RESTful routing; trace back to new.ejs and
+        // routes/tickets.js to see more detailed explanations
+
+        console.log(ticket);
+        res.redirect(`/flights/${req.params.id}`);
     });
 }
