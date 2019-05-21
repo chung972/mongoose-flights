093c1fadb5a6af4ccaea45f1b2878d2c4d538381
Albert Chung <chung972@gmail.com>
2019-05-20 19:05:16 -0500

    fixes formatting

--- routes/flights.js
+++ routes/flights.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 var flightsCtrl = require("../controllers/flights");
 
-/* GET /flights */
 router.get("/", flightsCtrl.index);
-/* GET /flights/new */
+router.get("/:id", flightsCtrl.show);
 router.get("/new", flightsCtrl.new);
 router.post("/", flightsCtrl.create);
-router.get("/:id", flightsCtrl.show);
 
 module.exports = router;
